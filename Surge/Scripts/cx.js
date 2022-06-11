e3c81971db85cc084dc06c8d140973dbc597553b
arlocc <107193123+arlocc@users.noreply.github.com>
2022-06-11 19:44:42 +0800

    Update cx.js

--- Surge/Scripts/cx.js
+++ Surge/Scripts/cx.js
@@ -1,23 +1,3 @@
-let body = $response.body
+var body = $response.body.replace('var isPay = document', 'var isPay = 1;  #').replace('var articleProperties', 'var articleProperties2').replace(/nickname\":\".*?\"/g, 'nickname\":\"已解锁\"').replace('isFree\":\\d', 'isFree\":0').replace('aType\":\\d', 'aType\":0').replace('feeType\":\\d', 'feeType\":0')
 
-if (/<\/html>|<\/body>/.test(body)) {
-  body = body.replace('</body>', `
-
-<script>
-const elecJSPack = function(){const payTips = document.querySelectorAll('.cons-pay-tip');
-  for (const payTip of payTips) {
-    payTip.removeAttribute('style');
-  }
-  const appContent = document.getElementById('appContent');
-  if (appContent) {
-    const pHidden = document.querySelectorAll('p:not([style="display:block;"]');
-    for (const el of pHidden) {
-      el.setAttribute('style', 'display:block;');
-    }
-  }
- $done()}()</script></body>`)
-
-  console.log('Success')
-}
-
-$done({ body })
+$done({ body });
