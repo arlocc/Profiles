374406a6851b25f82a5f30bedd58b36b300e047d
arlocc <107193123+arlocc@users.noreply.github.com>
2022-07-14 10:18:05 +0800

    Update setu.js

--- Surge/Scripts/setu.js
+++ Surge/Scripts/setu.js
@@ -1,8 +1,8 @@
-let apikey = '856426855e95d8e0e16da5' //请在 Telegram 内使用 @loliconApiBot 申请，
-let r18 = 2 //18禁为1 非为0 2是混合
+let apikey = '' //请在 Telegram 内使用 @loliconApiBot 申请，
+let r18 = 1 //18禁为1 非为0 2是混合
 let keyword = ''//搜索关键字
 var params = {
-    url:encodeURI(https://api.lolicon.app/setu?apikey=" + apikey + "&r18=" + userR18 + "&keyword=" + userKeyword + "&num=" + userNum),
+    url:encodeURI(https://api.lolicon.app/setu/v2?size=original&size=regular),
     header:{  
      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Mobile/15E148 Safari/604.1"
     },
@@ -13,7 +13,7 @@ $httpClient.get(params,function(error, response, data){
     let msg = datas.data[0]
     let url = msg.url
     console.log(url)
-    $notification.post("每日色图","",msg.tags,{'openUrl':url,'mediaUrl':url})
+    $notification.post("极速开车","",msg.tags,{'openUrl':url,'mediaUrl':url})
    } 
    else{
       console.log(datas.msg)
