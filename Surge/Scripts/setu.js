aa27513662060c556b8cc30e2bf2423f4401429a
arlocc <107193123+arlocc@users.noreply.github.com>
2022-07-14 09:48:06 +0800

    Update setu.js

--- Surge/Scripts/setu.js
+++ Surge/Scripts/setu.js
@@ -2,7 +2,7 @@ let apikey = '856426855e95d8e0e16da5' //请在 Telegram 内使用 @loliconApiBot
 let r18 = 2 //18禁为1 非为0 2是混合
 let keyword = ''//搜索关键字
 var params = {
-    url:encodeURI("https://api.lolicon.app/setu?apikey=" + apikey + "&r18=" + r18 + "&keyword=" + keyword),
+    url:encodeURI("https://api.lolicon.app/setu/v2?apikey=" + apikey + "&r18=" + r18 + "&keyword=" + keyword),
     header:{  
      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Mobile/15E148 Safari/604.1"
     },
